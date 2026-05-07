7f411526bd3ac145446d87dba3dd234fbaa71b44
Angel <sevinchoyazimboevna@gmail.com>
2026-05-08 01:29:49 +0500

    BRR: PYTHON SYSTEM VARIABLE AND SPECIAL METHODS VIA DUNDER

--- run.py
+++ run.py
@@ -1,5 +1,21 @@
-a = 100
-print("a:", a)
+# a = 100
+# print("a:", a)
 
-message = "Hello World!"
+# message = "Hello World!"
+# print(message)
+
+message = "Python"
 print(message)
+
+result = type(message)
+print(result)
+
+# Dunder - __builtins__, __init__
+
+'''
+IN python , there are builtin tools:
+(1) TYPES > int float str list dict
+(2) FUNCTION > print() len() input() type() str() int()
+(3) CONSTANTA > TRue False None
+'''
+print(dir(__builtins__))
