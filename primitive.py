c25e765cc8720694d623681ca7b50b1876e0209f
Angel <sevinchoyazimboevna@gmail.com>
2026-05-08 01:52:42 +0500

    BRR : LEARN STRING

--- primitive.py
+++ primitive.py
@@ -11,3 +11,23 @@ print(f"the count: {count} and type: {counti}")
 result1 = count.bit_count()  #method
 result2 = count.numerator  #state
 print(result1, result2)
+
+
+print("===========STRING===========")
+#METHODS: upper() lower() title() find() replace()
+
+course = "AI Python Fullstack"
+result = type(course)
+print(f"result1: {result}")
+
+result = course.title()
+print(f"result2: {result}")
+
+result = course.upper()
+print(f"result3: {result}")
+
+result = course.replace("Fullstack", "MasterClass")
+print(f"result4: {result}")
+
+print("===========BOOLEAN===========")
+
