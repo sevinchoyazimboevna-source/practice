eec93cfa015c4fc309a735890de5cb0fd6f185d1
Angel <sevinchoyazimboevna@gmail.com>
2026-05-12 02:20:54 +0500

    BRR: LEARN PYTHON OBJECTS

---
+++ objects.py
@@ -0,0 +1,29 @@
+'''
+OBJECTS:
+1 WHAT IS OBJECT
+2 ITERABLE OBJECTS & RANGE
+3 DDICTIONARY
+4 ERROR HANDING SYSTEM
+'''
+
+import array #package/modul
+import math
+from math import ceil, asin
+
+print("==========WHAT IS OBJECT===========")
+# an object has state and method properties
+# everything is object
+
+print(type('Hello Mit!'))
+print(type(100))
+print(type(True))
+print(type(array))
+print(type(math))
+
+
+ 
+result1 = math.ceil(97.7) #call
+print("result1: ", result1)
+
+result2 = ceil(99.9)
+print("result2: ", result2)
