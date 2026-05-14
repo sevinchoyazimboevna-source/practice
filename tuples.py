28bf7a0b4c34ce2952b1afdbb85ddd53a8e4a6cd
Angel <sevinchoyazimboevna@gmail.com>
2026-05-15 03:57:10 +0500

    FEAT: TUPLE AND LIST

---
+++ tuples.py
@@ -0,0 +1,24 @@
+'''
+TUPLES:
+1 WHAT IS TUPLE: TYPE VS LIST
+2 UNPACKING ARGUMENT
+3 ZIP
+'''
+print("===========WHAT IS TUPLE: TYPE VS LIST=============")
+#java/php/nodejs array => python list
+
+#literal
+numbs = [3,4,7,1]
+print(numbs)
+
+#constructer
+letter = list("Hello World")
+fruits = ["apple", "banana", "kiwi"]
+print("before:", fruits)
+
+#tuplle ni ichidaki qiymatni ozgartirib bolmidi
+#Tuple
+animals = ("cat", "dog", "fish", "lion")
+tuple_obj = ("MIT", 100, True, None)
+
+print(animals[0])
