1b852e2c29ed8c0ac7ecbdb668e7cc3f011b4d6d
Angel <sevinchoyazimboevna@gmail.com>
2026-05-19 03:44:47 +0500

    FIX: SPECIFIC OPERATORS WITH SET

--- array.set.py
+++ array.set.py
@@ -28,3 +28,33 @@ print(numbers)
 # katta son bolsa array mashinani kamroq kuchlantirish uchun
 # list esa ososnroq sonlar uchun
 
+
+# SET
+# set of unique collection without keeping order
+# index tushunchasi yok
+new_numbers = array("i", [1,4,5,7,8,4 ,7 , 5, 47])
+numbs_set = set(new_numbers)
+print(numbs_set)
+# set matigida doyin gulli qavs paydo boladi
+# setlarda ketma ketlik mavjud emas
+
+print(f"numbs: {numbs_set} and type: {type(numbs_set)}")
+
+numbs_set.add(200)
+print(numbs_set)
+
+numbs_set.add(7)
+print(numbs_set)
+
+print("+==============SPECIFIC OPERATORS WITH SET==================")
+
+# | & - ^ 
+
+a = {10, 20, 50}
+b = {20, 40}
+
+result1 = a | b #union  #ikkalasida bor bir hil qiymatni faqat bittasinikini  kaytaradi
+result1 = a & b #intersection bir hil qiymatni qayataradi
+result1 = a - b #difference
+result1 = a ^ b #symetric difference bir hil qaytarimagan qiymatni olmay qolganlarni oladi
+print(result1)
