b5b15751a8668495a8c4a342fc8b8e478736a8f2
Angel <sevinchoyazimboevna@gmail.com>
2026-05-19 06:18:05 +0500

    FEAT: SIMPLE EXAMPLE

---
+++ debugging.py
@@ -0,0 +1,22 @@
+'''
+Packagesw and Debugging:
+1 Python Packages and Core Package.
+2 Package Manager and External Package.
+3 Debugging.
+'''
+
+print("===========Python Packages and Core Package.============")
+# python > package/modul 
+# CORE>FILE>EXTERNALL
+
+
+print("CORE----------------")
+import turtle
+
+t = turtle.Turtle()
+t.shape("turtle") 
+t.speed(2)
+t.circle(100)
+
+turtle.done()
+
