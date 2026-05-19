721891b38cebb8fed2bd7c2c67f68deb9429b26c
Angel <sevinchoyazimboevna@gmail.com>
2026-05-19 06:39:20 +0500

    FIX: CORE PACKAGES STUDY

--- debugging.py
+++ debugging.py
@@ -4,14 +4,12 @@ Packagesw and Debugging:
 2 Package Manager and External Package.
 3 Debugging.
 '''
-
+import turtle
 print("===========Python Packages and Core Package.============")
 # python > package/modul 
 # CORE>FILE>EXTERNALL
 
-
-print("CORE----------------")
-import turtle
+#CORE PACKAGE
 
 t = turtle.Turtle()
 t.shape("turtle") 
@@ -20,3 +18,17 @@ t.circle(100)
 
 turtle.done()
 
+print("===========")
+my_file = open("material/message.txt", "r") #open syntax orqali turli malumotlarni oqishimiz mumkin
+try:
+    content = my_file.read()
+    print("content:", content)
+finally:
+    my_file.close()
+
+#with - context manager - ishimizni yengilashtiradi 
+with  open("material/message.txt", "r") as your_file:
+    your_content = your_file.read()
+    print("your file:", your_content)
+
+print("DONE")
